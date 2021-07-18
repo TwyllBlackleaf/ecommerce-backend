dfee3ab79868583e1a0a87f119c78e746a48b95d
Twyll <twyllblackleaf@gmail.com>
2021-07-18 18:12:31 -0400

    finish product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,14 +21,29 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
-    // find all products
-    // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-    // find a single product by its `id`
-    // be sure to include its associated Category and Tag data
+    Product.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Category
+            },
+            {
+                model: Tag,
+                as: "product_tags"
+            }
+        ]
+    })
+        .then(data => res.json(data))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
 });
 
 // create new product
@@ -106,7 +121,16 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-    // delete one product by its `id` value
+    Product.destroy({
+        where: {
+            id: req.params.id,
+        }
+    })
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
